583250e80ae7b8e08d855a5bf3e7c935519016d1
mihir224 <mihirsaini25@gmail.com>
2023-03-29 13:48:51 +0530

    home component for cards

---
+++ client/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+
+function Home(){
+    return(
+        <div id="cards">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
+export default Home;
