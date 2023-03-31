a159bfab0017c804dba3e1d550a038265b0b1de4
mihir224 <mihirsaini25@gmail.com>
2023-04-01 03:13:29 +0530

    user token verification

---
+++ server/verifyToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+export const verifyToken=(req,res,next)=>{
+    const token=req.cookies.access_token;
+    if(!token){
+        return next(createError(401,"You are not authorised"));
+    }
+    jwt.verify(token,process.env.JWT,(err,decodedData)=>{
+        if(err){
+            return next(createError(403,"Token invalid"));
+        }
+        req.data=decodedData; //user is verified, thus saving the data inside the token which contains the user's id
+        next(); //to continue where we left off after verification
+    });
+}
