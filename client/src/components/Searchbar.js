6d48e2f9d38b4d6373715624c2fda25ccb37c68d
mihir224 <mihirsaini25@gmail.com>
2023-04-19 20:42:40 +0530

    configured redux to manage user data

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -7,20 +7,20 @@ import SearchIcon from '@mui/icons-material/Search';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import { useSelector } from "react-redux";
 
 function Searchbar(){
+    const currentUser=useSelector(state=>state.user.currentUser); 
     return (
         <div id="search-bar">
             <div id="search-area">
             <input id="search" type="search" placeholder="Search"></input>
             <button id="search-btn" type="submit"><SearchIcon id="search-icon"/></button>
             </div>
-            <Link to="signin" style={{textDecoration: "none",fontSize: "14px",listStyle:"none"}}><button id="signin-btn" type="submit"><AccountCircleRoundedIcon/><span>Sign in</span></button></Link>
-            {/* <div id="n-icons">
-                <VideoCallIcon id="icon"/>
-                <NotificationsIcon id="icon"/>
-                <button type="button"></button>
-            </div> */}
+            {currentUser?
+            <div id="n-icons"><VideoCallIcon id="icon"/><NotificationsIcon id="icon"/><button type="button"></button></div>
+            :<Link to="signin" style={{textDecoration: "none",fontSize: "14px",listStyle:"none"}}><button id="signin-btn" type="submit"><AccountCircleRoundedIcon/><span>Sign in</span></button></Link>
+            }
         </div>
     )
 }
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,19 +1,38 @@
 import React from "react";
 import "../styles/SignIn.css";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
 
 function SignIn(){
+    const dispatch=useDispatch();
+    const [name,setName]=React.useState("");
+    const [email,setEmail]=React.useState("");
+    const [password,setPassword]=React.useState("");
+    const handleLogin=async(event)=>{
+        event.preventDefault();
+        dispatch(loginStart())
+        try{
+            const res=await axios.post("/auth/signin",{name,password}); //second parameter here is the request body
+            dispatch(loginSuccess(res.data))
+            //console.log(res.data)
+        }catch(err){
+            dispatch(loginFailure(err));
+            //console.log(err)
+        }
+    }
     return (
         <div id="sign-in">
         <h2>Sign in</h2>
         <p>to continue to YouTube</p>
-        <input type="email" id="email" name="email" placeholder="Email"></input>
-        <input type="password" id="password" name="password" placeholder="Enter your password"></input>
-        <button type="submit">Sign in</button>
+        <input type="text"  name="username" placeholder="Username" onChange={(event)=>{setName(event.target.value)}}></input>
+        <input type="password"  name="password" placeholder="Enter your password" onChange={(event)=>{setPassword(event.target.value)}}></input>
+        <button type="submit" onClick={handleLogin}>Sign in</button>
         <p>OR</p>
         <h2>Sign up</h2>
-        <input type="text" id="username" name="username" placeholder="Username"></input>
-        <input type="email" id="email" name="email" placeholder="Email"></input>
-        <input type="password" id="password" name="password" placeholder="Enter your password"></input>
+        <input type="text"  name="username" placeholder="Username" onChange={(event)=>{setName(event.target.value)}}></input>
+        <input type="email"  name="email" placeholder="Email" onChange={(event)=>{setEmail(event.target.value)}}></input>
+        <input type="password"  name="password" placeholder="Enter your password" onChange={(event)=>{setPassword(event.target.value)}}></input>
         <button type="submit">Sign up</button>
         </div>
     )
