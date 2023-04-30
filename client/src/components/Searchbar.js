511c95f98247ed128f13ab9b580c3e3ae71b7835
mihir224 <mihirsaini25@gmail.com>
2023-04-30 17:15:05 +0530

    update searchbar on user login

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Searchbar.css";
 import { Link } from "react-router-dom"
 import logo from "../images/logo.png";
@@ -7,24 +7,49 @@ import SearchIcon from '@mui/icons-material/Search';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
-import { useSelector } from "react-redux";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/userSlice";
 
 function Searchbar(){
+    const dispatch=useDispatch();
     const currentUser=useSelector(state=>state.user.currentUser); 
+    // const [dropdown,setDropDown]=useState(false);
     const customStyling={
-        background:`url(${currentUser.img}) no-repeat`,
+        background:`url(${currentUser?.img}) no-repeat`,
         backgroundSize:"40px",
         backgroundPosition:"-5px -2px"
-        
+    }
+   
+    const handleLogout=(event)=>{
+        event.preventDefault();
+        dispatch(logout());
     }
     return (
-        <div id="search-bar">
+        <div id="search-bar" >
+        <div id="logo-container">
+                <div id="hamburger"><MenuIcon id="icon"/></div>
+                <Link to="/" style={{textDecoration: "none",fontSize: "14px"}}><div id="logo">
+                    <img src={logo} height="25" width="40"></img><h2>MS-Tube</h2>
+                </div></Link>
+            </div>
             <div id="search-area">
             <input id="search" type="search" placeholder="Search"></input>
             <button id="search-btn" type="submit"><SearchIcon id="search-icon"/></button>
             </div>
             {currentUser?
-            <div id="n-icons"><VideoCallIcon id="icon"/><NotificationsIcon id="icon"/><button type="button" style={customStyling}></button></div>
+            <div id="n-icons">
+                <VideoCallIcon id="icon"/>
+                <NotificationsIcon id="icon"/>
+               <div id="dropdown">
+                <button type="button"  style={customStyling}></button>
+                <div id="dropdown-content" >
+                    <ul>
+                    <li className="dd-list-item"><button id="logout-btn" onClick={handleLogout}><LogoutIcon style={{fontSize:"20px"}}/> Log out</button></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
             :<Link to="signin" style={{textDecoration: "none",fontSize: "14px",listStyle:"none"}}><button id="signin-btn" type="submit"><AccountCircleRoundedIcon/><span>Sign in</span></button></Link>
             }
         </div>
