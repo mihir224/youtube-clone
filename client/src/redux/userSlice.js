2253ff43cc82e3598ddda7fc861ee1871a307b07
mihir224 <mihirsaini25@gmail.com>
2023-05-04 01:10:28 +0530

    like, subscribe, dislike actions

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -23,11 +23,31 @@ const userSlice=createSlice({
             state.currentUser=null;
             state.isLoading=false;
             state.serverErr=null;
+        },
+        // subscribe:(state,action)=>{
+        //     if(!state.currentUser.subscribedUsers.includes(action.payload)){
+        //         state.currentUser.subscribedUsers.push(action.payload)
+                
+        //     }
+        // },
+        // unsubscribe:(state,action)=>{
+        //     if(state.currentUser.subscribedUsers.includes(action.payload)){
+        //         state.currentUser.subscribedUsers.splice(state.currentUser.subscribedUsers.findIndex(userId=>userId===action.payload),1)
+               
+        //     }
+        // }
+        subscribe:(state,action)=>{
+            if(state.currentUser.subscribedUsers.includes(action.payload)){
+                state.currentUser.subscribedUsers.splice(state.currentUser.subscribedUsers.findIndex(channelId=>channelId===action.payload),1);
+            }
+            else if(!state.currentUser.subscribedUsers.includes(action.payload)){
+                state.currentUser.subscribedUsers.push(action.payload);
+            }
         }
     }
 })
 
-export const {loginStart,loginSuccess,loginFailure,logout} = userSlice.actions;
+export const {loginStart,loginSuccess,loginFailure,logout,subscribe,unsubscribe} = userSlice.actions;
 
 export default userSlice.reducer;
 
--- client/src/redux/videoSlice.js
+++ client/src/redux/videoSlice.js
@@ -18,10 +18,23 @@ const videoSlice=createSlice({
         fetchError:(state,action)=>{
             state.isLoading=false
             state.serverErr=action.payload
+        },
+        //we use like and dislike actions to immediately update the like/dislike icon as soon as the user clicks on it instead of him having to see it only after reloading the page
+        like:(state,action)=>{
+            if(!state.currentVideo.likes.includes(action.payload)){
+                state.currentVideo.likes.push(action.payload);
+                state.currentVideo.dislikes.splice(state.currentVideo.dislikes.findIndex(userId=>userId===action.payload),1); //to remove the user from dislikes array if he previously disliked the video
+            }
+        },
+        dislike:(state,action)=>{
+            if(!state.currentVideo.dislikes.includes(action.payload)){
+                state.currentVideo.dislikes.push(action.payload);
+                state.currentVideo.likes.splice(state.currentVideo.likes.findIndex(userId=>userId===action.payload),1); //to remove the user from likes array if he previously liked the video
+            }
         }
     }
 })
 
-export const {fetchStart,fetchSuccess,fetchError}=videoSlice.actions;
+export const {fetchStart,fetchSuccess,fetchError,like,dislike}=videoSlice.actions;
 
 export default videoSlice.reducer;
