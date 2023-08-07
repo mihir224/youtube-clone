116a75287010635647142f60076f05f525b37d5c
mihir224 <mihirsaini25@gmail.com>
2023-08-07 05:32:23 +0530

    added upload functionality

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,14 +1,87 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import '../styles/Upload.css';
 import CloseIcon from '@mui/icons-material/Close';
 import VideoCallIcon from '@mui/icons-material/VideoCallOutlined';
 import ImageIcon from '@mui/icons-material/Image';
 import { useDispatch, useSelector } from "react-redux";
-
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from '../firebase';
+import axios from 'axios';
+import {Navigate} from "react-router-dom";
 
 
 function Upload({setUploadOpen}){
     const dispatch=useDispatch();
+    const [img,setImg]=useState(undefined);
+    const [video,setVideo]=useState(undefined);
+    const [imgPerc,setImgPerc]=useState(0);
+    const [videoPerc,setVideoPerc]=useState(0);
+    const [input,setInput]=useState({});
+    const [tags,setTags]=useState([])
+
+    const uploadFile=(file,type)=>{
+        const storage = getStorage(app);
+        const filename=new Date().getTime() + file.name;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed',
+        (snapshot) => {
+            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            console.log('Upload is ' + progress + '% done');
+            type==='imgUrl'?setImgPerc(progress):setVideoPerc(progress);
+            switch (snapshot.state) {
+            case 'paused':
+                console.log('Upload is paused');
+                break;
+            case 'running':
+                console.log('Upload is running');
+                break;
+            default:
+                break;
+            }
+        }, 
+        (error) => {},
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setInput(prev=>{
+                return {
+                    ...prev,
+                    [type]:downloadURL
+                }
+            })
+            });
+        }
+        );
+    }
+    useEffect(()=>{
+        img && uploadFile(img,"imgUrl");
+    },[img]);
+
+    useEffect(()=>{
+        video && uploadFile(video,"videoUrl");
+    },[video]);
+
+    const handleChange=(e)=>{
+        setInput(prev=>{
+            return {
+                ...prev,
+                [e.target.name]:e.target.value
+            }
+        })
+    }
+    const handleTags=(e)=>{
+        setTags(e.target.value.split(","));
+    }
+
+    const handleUpload=async(e)=>{
+        e.preventDefault();
+        const res=await axios.post('/videos/add',{...input,tags});
+        dispatch(setUploadOpen(false));
+        if (res.status === 200) {
+        return <Navigate to={`/video/${res.data._id}`} replace={true} />;
+        }
+    }
+
     return (
         <div id='upload'>
             <div>
@@ -17,21 +90,25 @@ function Upload({setUploadOpen}){
             </div>
             <div className='upload-div'>
             <label className='file-label' htmlFor='uploaded-video'><span id='upload-text'>Choose Video</span> <VideoCallIcon className='upload-icon'/></label>
-            <input id='uploaded-video' className='file-input' type='file' accept='video/*' />
+            {videoPerc>0?<span style={{fontSize:'14px'}}>Uploading {videoPerc}%</span>:<input id='uploaded-video' className='file-input' type='file' accept='video/*' onChange={e=>setVideo(e.target.files[0])} />}
             </div>
             <div>
             <label className='text-label' htmlFor='vid-title'>Title:</label>
-            <input id='vid-title' type='text' className='input-text' placeholder='Video Title'/>
+            <input id='vid-title' type='text' className='input-text' name='title' onChange={handleChange} placeholder='Video Title' required={true}/>
             </div>
             <div>
             <label className='text-label' htmlFor='uploaded-desc'>Description:</label>
-            <input id='uploaded-desc' className='input-text' type='text' placeholder='Video Description'/>
+            <input id='uploaded-desc' className='input-text' type='text' name='desc' onChange={handleChange} placeholder='Video Description' required={true}/>
+            </div>
+            <div>
+            <label className='text-label' htmlFor='uploaded-tags'>Tags:</label>
+            <input id='uploaded-tags' className='input-text' type='text' name='tags' onChange={handleTags} placeholder='Tags (use comma in between)'/>
             </div>
             <div className='upload-div'>
             <label className='file-label' htmlFor='uploaded-thumbnail'><span id='upload-text'>Pick Thumbnail</span> <ImageIcon/></label>
-            <input id='uploaded-thumbnail' className='file-input' type='file' accept='image'/>
+            {imgPerc>0?<span style={{fontSize:'14px'}}>Uploading {imgPerc}%</span>:<input id='uploaded-thumbnail' className='file-input' type='file' accept='image' onChange={e=>setImg(e.target.files[0])}/>}
             </div>
-            <button id="upload-btn" type="submit"> <span>Upload</span></button>
+            <button id="upload-btn" type="submit" onClick={handleUpload}> <span>Upload</span></button>
         </div>
     )
 }
