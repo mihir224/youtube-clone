0e5d8f152ac06e3c16d4bbb61228b243fd027b95
mihir224 <mihirsaini25@gmail.com>
2024-05-17 12:27:18 +0530

    added copyright footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import Video from './components/Video';
 import Search from './components/Search';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         <Route path="signin" element={<SignIn/>}/>
       </Route>
     </Routes>
+    <Footer/>
     </div>
     
     </div>
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer(){
+    return (
+        <div id='footer'>
+            ©Copyright Mihir Saini 2024. All rights reserved. 
+        </div>
+    )
+}
+
+export default Footer;
